0cd99f43b3605a764560f862dad2c5c2c4767632
Arik Nur Khoiriyah <ariknurkhoiriyah@gmail.com>
2017-03-13 19:57:21 +0700

    Desain

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl60517283033/deardiary/temp2.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project2.xirpl60517283033.deardiary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class temp2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp2);
+    }
+}
